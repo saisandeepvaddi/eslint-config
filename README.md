a20a60ac95786ec5b31c01867f09a756fb6a186b
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2020-07-11 15:30:06 -0400

    first config

---
+++ README.md
@@ -0,0 +1,11 @@
+# @saisandeepvaddi/eslint-config
+
+Shareable eslint configs for my personal projects. Check [index.js](/index.js) for rules.
+
+You can use it by updating your `.eslintrc` or `.eslintrc.js` with
+
+```js
+{
+  "extends": "@saisandeepvaddi/eslint-config"
+}
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  extends: [
+    "prettier",
+    "prettier/react",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
+  plugins: ["prettier", "react", "react-hooks"],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "array-callback-return": "off",
+    "no-console": "off",
+    "no-debugger": "error",
+    "no-undef": "error",
+    "no-unused-vars": [2, { args: "after-used", argsIgnorePattern: "^_" }],
+    eqeqeq: "error",
+    "no-var": "error",
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "linebreak-style": "off",
+    "no-nested-ternary": "off",
+    "consistent-return": "off",
+    "no-use-before-define": "error",
+    "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/extensions": [".js", ".jsx", ".css", ".scss", ".png"],
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".css", ".scss", ".png"],
+      },
+    },
+  },
+  globals: {
+    // NextJs does not require you to import React into each component. so suppress errors for missing 'import React' in files.
+    React: "writable",
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+};
